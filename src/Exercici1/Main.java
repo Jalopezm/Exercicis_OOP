5ef8302a73603cca42020559654bf52853af3e81
Jalopezm <jalopezm@esliceu.net>
2022-09-29 19:26:40 +0200

    Exercici1

--- src/Exercici1/Main.java
+++ src/Exercici1/Main.java
@@ -27,5 +27,28 @@ public class Main {
             System.err.println("Valores Introducidos No Validos");
         }
 
+        Customer customer1 = new Customer("Joan");
+        //Probamos a utilizar un tipo de miembro valido y uno no valido
+        //si de
+        try {
+            /**customer.setMemberType("Premiun")**/
+            customer.setMemberType("Gold");
+            System.out.println(customer.toString());
+        } catch (CustomerError e) {
+            System.err.println("No Member Type Coincidences");
+        }
+        Visit visit1 = new Visit(customer, new Date());
+
+        try {
+            visit.setServiceExpense(21.34);
+            visit.setProductExpense(39);
+            System.out.println(visit.toString());
+            System.out.println("Descuento Servicio " + DiscountRate.getServiceDiscountRate(customer.getMemberType()));
+            System.out.println("Descuento Productos " + DiscountRate.getProductDiscountRate(customer.getMemberType()));
+            System.out.println("Total Gastado " + visit.getTotalExpense());
+        } catch (VisitError e) {
+            System.err.println("Valores Introducidos No Validos");
+        }
+
     }
 }
